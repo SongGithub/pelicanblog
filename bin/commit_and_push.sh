d63d5680df26d5bda351983d9f6950c14cad721c
Travis on behalf of Song <songjin@hotmail.com>
2018-05-24 21:44:11 +1000

    udpate bin file

--- bin/commit_and_push.sh
+++ bin/commit_and_push.sh
@@ -25,12 +25,10 @@ cd output
 # default branch is none but a commit. So have to checkout master branch before
 # writting files into it via `make publish`. I know it looks hacky now.
 git checkout master
-# get rid of historical rubbish that are not relevant, but preserve CNAME file
-# that is created automatically by Github when you configure it with your domain
-rm -rf *
 
 cd ..
 make publish
+# CNAME file that is required by Github when you configure it with your domain
 if [ -f CNAME ]; then
   cp CNAME output/
 fi
